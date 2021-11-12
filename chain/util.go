449df7a1f013e0bae5e6ad1e71134c82224ea7a7
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-11-12 14:51:53 -0800

    SE punctuation

--- chain/util.go
+++ chain/util.go
@@ -2,7 +2,8 @@ package chain
 
 import "strings"
 
-var punctuation = []byte{'.', ',', '?', '!', '\'', '*', '`', '~', '_', '\n', ':', '"'}
+var punctuation = []byte{'.', ',', '?', '!', '\'', '*', '`', '~', '_', '\n', '"'}
+var startendpunctuation = []byte{':'}
 
 func simplify(msg string) string {
 	msg = strings.ToLower(msg)
@@ -10,5 +11,31 @@ func simplify(msg string) string {
 	for _, val := range punctuation {
 		msg = strings.ReplaceAll(msg, string(val), "")
 	}
+
+	// Start end punctuation
+	containse := false
+	for _, val := range startendpunctuation {
+		if strings.Contains(msg, string(val)) {
+			containse = true
+		}
+	}
+	if containse {
+		words := strings.Split(msg, " ")
+		for i, word := range words {
+			changed := false
+			if word[0] == ':' {
+				word = word[1:]
+				changed = true
+			}
+			if word[len(word)-1] == ':' {
+				word = word[:len(word)-1]
+				changed = true
+			}
+			if changed {
+				words[i] = word
+			}
+		}
+	}
+
 	return msg
 }
